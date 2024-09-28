469399956260fc1bd9ae5b02962082374f369065
Solly <solobarine@gmail.com>
2024-09-28 14:02:02 +0100

    Create Admin  Middleware for Admin Routes

---
+++ backend/app/Http/Middleware/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user() || !in_array($request->user()->role, ['admin', 'super_admin'])) {
+
+            return response()->json([
+                'status' => false,
+                'error'  => 'Unauthorized Request'
+            ], 401);
+        };
+
+        return $next($request);
+    }
+}
